6ea1e825d3b09d253d8fd127b8501ca308cdd401
Harshal Shinde <shindeharshal009@gmail.com>
2022-06-26 03:50:30 +0530

    Form Validation

---
+++ Assignment/FormA14/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormGroup,FormBuilder,FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RequiredValidator,PatternValidator } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent
+{
+  constructor(private _fbobj : FormBuilder)
+  {}
+
+  MyForm = this._fbobj.group(
+  {
+    FirstName : ['',Validators.required],
+    LastName : ['',Validators.required],
+    Email : ['',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+    Phone : ['',[Validators.required,Validators.pattern("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$")]],
+    Address : ['',[Validators.required,Validators.minLength(10)]]
+
+  }
+  );
+
+
+}
