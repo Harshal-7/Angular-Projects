2a4221eee5c6e939999e90c78701214a795204c7
Harshal Shinde <shindeharshal009@gmail.com>
2023-02-16 17:30:54 +0530

    Tasks project updated with black background

--- Tasks/tailwind.config.js
+++ Tasks/tailwind.config.js
@@ -2,9 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        '2xl': '1px 1px 5px rgb(33 34 43 / 20%)',
+        '3xl': '0 0 3px rgba(0, 0, 0, .8), 0 0 5px rgba(0, 0, 0, .9)',
+      },
+    },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms',
+    'tailwindcss-textshadow'),
   ],
 }
